34a55405cf34bdd0a7cb736e4891fd2744c4d5fe
rk102 <rk10295@gmail.com>
2022-07-06 20:39:14 +0530

    update_added

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,26 @@
 $("#add_user").submit(function(event){
     alert("Data Inserted Successfully");
 })
+
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    var unindexedArray=$(this).serializeArray();
+    var data={}
+
+    $.map(unindexedArray,function(n,i){
+        data[n['name']]=n['value']
+    })
+
+    console.log(data);
+
+    var request={
+        "url":`http://localhost:8000/api/users/${data.id}`,
+        "method":"PUT",
+        "data":data
+    }
+
+    $.ajax(request).done(function(response){
+        alert("Data Updated Successfully");
+    })
+})
